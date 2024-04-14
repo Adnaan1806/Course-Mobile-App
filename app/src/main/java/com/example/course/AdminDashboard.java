bf66f5b168f262043cacafbe4b32a9944e33c749
Adnaan1806 <CODSE221F-067@student.nibm.lk>
2024-04-14 12:33:13 +0530

    UI changes 6 AD

--- app/src/main/java/com/example/course/AdminDashboard.java
+++ app/src/main/java/com/example/course/AdminDashboard.java
@@ -1,14 +1,29 @@
 package com.example.course;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AdminDashboard extends AppCompatActivity {
+
+    CardView D1,D2,D3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_dashboard);
+
+        D1 = findViewById(R.id.d1);
+
+        D1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminDashboard.this, AddCourse.class);
+                startActivity(intent);
+            }
+        });
     }
 }
